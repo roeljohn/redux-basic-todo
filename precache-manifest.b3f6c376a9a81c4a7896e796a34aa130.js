1cd15bd9ff3c4e36b424a84f0e4d055b9e68b73e
Roel <bobisroeljohn@gmail.com>
2019-10-25 22:39:28 +0800

    Updates

---
+++ precache-manifest.b3f6c376a9a81c4a7896e796a34aa130.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4dd5a794bdbb96f347f948e67c7e13b2",
+    "url": "/redux-basic-todo/index.html"
+  },
+  {
+    "revision": "1ef41132f67a32e258b5",
+    "url": "/redux-basic-todo/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "da6dd42b528ceecd88d9",
+    "url": "/redux-basic-todo/static/css/main.d8bc6d21.chunk.css"
+  },
+  {
+    "revision": "1ef41132f67a32e258b5",
+    "url": "/redux-basic-todo/static/js/2.acf34efd.chunk.js"
+  },
+  {
+    "revision": "da6dd42b528ceecd88d9",
+    "url": "/redux-basic-todo/static/js/main.75711193.chunk.js"
+  },
+  {
+    "revision": "89d1310f6174ba89f17f",
+    "url": "/redux-basic-todo/static/js/runtime-main.60168b6b.js"
+  }
+]);
